74036c5892f11d1f59cf4d43792be76a484cad4f
Konstantin Epifanov <epifanov.lab@gmail.com>
2023-07-13 14:47:13 +0300

    go live / end streaming button added

--- lib/apivideo/apivideo_page.dart
+++ lib/apivideo/apivideo_page.dart
@@ -178,9 +178,57 @@ class _ApiVideoPageState extends State<ApiVideoPage> with WidgetsBindingObserver
                 ],
               ),
             ),
-          )
-          //TODO: go live/end stream button
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+              child: GoLiveEndStreamButtonWidget(
+                isStreaming: _isStreaming,
+                onTap: _isStreaming ? onStopStreamingButtonPressed : onStartStreamingButtonPressed,
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class GoLiveEndStreamButtonWidget extends StatelessWidget {
+  final bool isStreaming;
+  final Function onTap;
+
+  const GoLiveEndStreamButtonWidget({
+    Key? key,
+    required this.isStreaming,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 48,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: Material(
+          color: isStreaming ? Colors.white : Colors.blue,
+          child: InkWell(
+            onTap: () => onTap(),
+            borderRadius: BorderRadius.circular(24),
+            child: Center(
+              child: Text(
+                isStreaming ? 'End streaming' : 'Go live',
+                style: TextStyle(
+                  color: isStreaming ? Colors.blue : Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
